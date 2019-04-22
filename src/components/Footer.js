c2f55da881b71c523e180c83c7b93499ceb87b34
linkswapnil <swapnilnavalakha2010@gmail.com>
2019-04-22 10:34:52 +0530

    Change footer
    Footer change

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.scss";
+export const Footer = props => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Header } from "./components/Header";
-import "./styles.css";
+import { Footer } from "./components/Footer";
+import "./styles.scss";
 
 function App() {
   return (
     <>
       <Header />
-      <main role="main" class="container">
+      <main role="main" className="container">
         <div className="App">
           <h1>Hello CodeSandbox</h1>
-          <h2>Start editing to see some magic happen!</h2>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start testediting to see some magic happen!</h2>
         </div>
       </main>
+      <Footer />
     </>
   );
 }
