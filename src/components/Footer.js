fbaa0aee9573fe2c4ab1054d48671d1b1f86b1b0
linkswapnil <swapnilnavalakha2010@gmail.com>
2019-04-24 15:54:53 +0530

    add copyright logo
    add copyright logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Footer.scss";
+import { Row, Container, Col } from "react-bootstrap";
 export const Footer = props => {
   return (
     <div className="footer">
-      <div className="row justify-content-center">
-        <div className="list">
+      <Row>
+        <Col md={2} lg={2} xl={2} sm={3} xs={3} className="list text-center">
           <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Title</h5>
           <ul className="list-unstyled">
             <li>
@@ -14,8 +15,8 @@ export const Footer = props => {
               <a href="#!">Link 2</a>
             </li>
           </ul>
-        </div>
-        <div className="list">
+        </Col>
+        <Col md={2} lg={2} xl={2} sm={3} xs={3} className="list text-center">
           <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Title</h5>
           <ul className="list-unstyled">
             <li>
@@ -25,19 +26,13 @@ export const Footer = props => {
               <a href="#!">Link 2</a>
             </li>
           </ul>
-        </div>
-        <div className="list">
-          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Title</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+        </Col>
+      </Row>
+      <Row className="justify-center">
+        <Col className="list text-center">
+          © Copyright <strong>HP</strong>. All Rights Reserved
+        </Col>
+      </Row>
     </div>
   );
 };
