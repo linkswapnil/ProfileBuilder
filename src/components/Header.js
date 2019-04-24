a5384950caf9955ba951bbf34e927921f313d328
linkswapnil <swapnilnavalakha2010@gmail.com>
2019-04-24 11:23:03 +0530

    Nav bar fix
    Nav bar fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,39 @@ import React from "react";
 import "./Header.scss";
 import { Navbar, Nav, Button } from "react-bootstrap";
 
-export const Header = props => {
-  return (
-    <Navbar
-      className="head"
-      collapseOnSelect
-      expand="lg"
-      bg="dark"
-      variant="dark"
-    >
-      <Navbar.Brand href="#home">
-        <img
-          alt="hp-logo"
-          src="https://www8.hp.com/us/en/images/THP_S_K_RGB_150_LG_Ctcm2451096197_Ttcm245108559832_F.png"
-          width="30"
-          height="30"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link className="mr-t4 text-white" href="#deets">
-            New User
-          </Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            <Button variant="primary" size="sm">
-              Login
-            </Button>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+export class Header extends React.Component {
+  render() {
+    return (
+      <Navbar
+        fluid
+        className="head"
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="#home">
+          <img
+            alt="hp-logo"
+            src="https://www8.hp.com/us/en/images/THP_S_K_RGB_150_LG_Ctcm2451096197_Ttcm245108559832_F.png"
+            width="30"
+            height="30"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto text-right mr-2">
+            <Nav.Link className="mr-t4 text-white" href="#deets">
+              New User
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              <Button variant="primary" size="sm">
+                Login
+              </Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
